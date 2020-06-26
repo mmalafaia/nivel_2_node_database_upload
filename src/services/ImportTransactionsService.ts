d3aa670c3e8891abbf11105c0c26ec26a8b450f9
Manoel Malafaia <mmalafaia@gmail.com>
2020-06-26 17:26:48 -0300

    Partial Work on Import

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,68 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
+
 import Transaction from '../models/Transaction';
+
+interface TransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+interface TransactionsDTO {
+  transaction: Array<TransactionDTO>;
+}
 
 class ImportTransactionsService {
   async execute(): Promise<Transaction[]> {
-    // TODO
+    async function loadCSV(filePath: string): Promise<TransactionDTO[]> {
+      const readCSVStream = fs.createReadStream(filePath);
+
+      const parseStream = csvParse({
+        from_line: 2,
+        ltrim: true,
+        rtrim: true,
+      });
+
+      const parseCSV = readCSVStream.pipe(parseStream);
+
+      const lines = TransactionsDTO();
+
+      parseCSV.on('data', line => {
+        lines.push(line);
+      });
+
+      await new Promise(resolve => {
+        parseCSV.on('end', resolve);
+      });
+
+      return lines;
+    }
+
+    const csvFilePath = path.resolve(
+      __dirname,
+      '..',
+      'tmp',
+      'import_template.csv',
+    );
+
+    const data = loadCSV(csvFilePath);
+
+    console.log(data);
+
+    const trans = new Transaction();
+
+    trans.title = data[0].title;
+
+    const transactions = [];
+
+    transactions.push(trans);
+
+    console.log(transactions);
+
+    return transactions;
   }
 }
 
